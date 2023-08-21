d28decdcd408a5ca94941f2a9b14f51a08703de0
Yusef-H <yusefhalabi456@gmail.com>
2023-08-21 17:59:40 +0300

    implement responsive collapsible navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
 import '../styles/Navbar.css';
-import cartSvg from '../assets/cart.svg'
+import cartSvg from '../assets/cart.svg';
+import menuSvg from '../assets/menu.svg';
 
 function Navbar() {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+
   return (
     <nav className='navbar'>
-      <ul className='nav-list'>
+      <button className="menu"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}>
+        <img src={menuSvg} alt="menu" />
+      </button>
+      <ul className={isNavExpanded ? 'nav-list expanded' : 'nav-list'}>
         <Link to="/">Home</Link>
         <Link to="/Store">Store</Link>
         <Link to="/About">About</Link>
       </ul>
+
 
       <button className='cart-btn'>
         <img src={cartSvg} alt="Cart" className='cart-icon' />
