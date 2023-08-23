aa7e23f6988da073eecd98f9cacc93e4399a5c0e
Yusef-H <yusefhalabi456@gmail.com>
2023-08-23 23:00:36 +0300

    add storeitem buttons skeleton

--- src/components/StoreItem.jsx
+++ src/components/StoreItem.jsx
@@ -2,12 +2,22 @@ import React from 'react'
 import '../styles/Store.css'
 
 function StoreItem({ title, price, image }) {
+    const quantity = 0;
     return (
         <div className='item-card'>
             <h1>{title}</h1>
             <img src={image} alt="" />
             <h1>{price} $</h1>
-
+            <div className="btn-container">
+                {quantity == 0 ?
+                    <button className='add-cart-btn'>Add To Cart</button> :
+                    <div className='min-plus-container'>
+                        <button className='minus-btn'>-</button>
+                        {quantity}
+                        <button className='plus-btn'>+</button>
+                    </div>
+                }
+            </div>
         </div>
     )
 } { }
