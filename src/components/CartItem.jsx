10542dfd08b3804faa38bc84a8ced88519c17054
Yusef-H <yusefhalabi456@gmail.com>
2023-08-26 19:35:00 +0300

    improve cart functionality and styling when displayed

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -7,7 +7,14 @@ function CartItem({ id, quantity, storeItems }) {
     if (item == null) return null;
     return (
         <div className='cart-item-cont'>
-            {item.title}
+            {console.log(item)}
+            <img className='item-img' src={item.image} alt="image" />
+            <div className='title-price-cont'>
+                <h2 className='title'>{item.title} </h2>
+                <h3 className='quantity'>x{quantity}</h3>
+                <h3 className='price'>{item.price * quantity} $</h3>
+            </div>
+            <button className="remove-btn" onClick={() => removeFromCart(id)}>X</button>
         </div>
     )
 }
