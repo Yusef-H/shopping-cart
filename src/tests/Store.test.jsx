b495cda9e1f35d0deb5621351650f3684809e5d5
Yusef-H <yusefhalabi456@gmail.com>
2023-08-26 16:55:05 +0300

    add store tests to test button functionality

--- src/tests/Store.test.jsx
+++ src/tests/Store.test.jsx
@@ -3,6 +3,8 @@ import { render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import Store from '../components/Store';
 import { CartProvider } from '../context/CartContext';
+import userEvent from '@testing-library/user-event';
+import { beforeAll, beforeEach } from 'vitest';
 
 global.fetch = vi.fn();
 const mockData = [
@@ -51,6 +53,48 @@ describe('Store component', () => {
         });
     });
 
+
+    describe('store buttons', () => {
+        let btns;
+        let plusButton;
+        let minusButton;
+        let user;
+        beforeEach(async () => {
+            user = userEvent.setup();
+            render(<CartProvider><Store storeItems={mockData} setStoreItems={(mockData) => mockData} /></CartProvider>);
+            btns = await screen.findAllByRole('button', { name: "Add To Cart" });
+        })
+
+        it('renders add to cart button on item', async () => {
+            await waitFor(() => {
+                const btns = screen.getAllByRole('button', { name: "Add To Cart" });
+                expect(btns[0]).toBeInTheDocument();
+            })
+        })
+
+        it('renders plus and minus buttons after click', async () => {
+            await user.click(btns[0]);
+            plusButton = screen.getByRole('button', { name: "+" });
+            minusButton = screen.getByRole('button', { name: "-" });
+            expect(plusButton).toBeInTheDocument();
+            expect(minusButton).toBeInTheDocument();
+            expect(screen.getByText('1')).toBeInTheDocument();
+        })
+
+        it('updates counter when button is clicked', async () => {
+            await user.click(btns[0]);
+            plusButton = screen.getByRole('button', { name: "+" });
+            await user.click(plusButton);
+            await user.click(plusButton);
+            await user.click(plusButton);
+            expect(screen.getByText('4')).toBeInTheDocument();
+        })
+    })
+
+
+
+
+
     it('handles fetch error', async () => {
         // Mock the fetch function to reject with an error
         window.fetch = vi.fn(() => Promise.reject({ json: () => new Error('Fetch error') }));
@@ -62,4 +106,6 @@ describe('Store component', () => {
             expect(screen.getByText('Error')).toBeInTheDocument();
         });
     });
+
+
 });
