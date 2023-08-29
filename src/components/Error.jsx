aaf7ad147058f0d58be07ef5a3788a7ed8a409b1
Yusef-H <yusefhalabi456@gmail.com>
2023-08-29 21:32:29 +0300

    update and style error page

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import '../styles/error.css';
+import { Link } from 'react-router-dom';
 function Error() {
   return (
-    <div class='error'>Error</div>
+    <div class='error'>
+      <h1 className='error-title'>Error Page!</h1>
+      <p className='error-msg'>You've reached the error page, most likely you have typed a wrong url. You can reach home page here:</p>
+      <Link to="/home">Home</Link>
+    </div>
   )
 }
 
