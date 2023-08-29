c88780b151fa1dc2ee61232b0ae8698b4b2f9a0e
Yusef-H <yusefhalabi456@gmail.com>
2023-08-29 21:26:02 +0300

    add cart total functionality & style cart items

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,12 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../styles/Cart.css'
 import { useCart } from '../context/CartContext'
 import CartItem from './CartItem'
 function Cart({ storeItems }) {
-    const { hidden, cartItems } = useCart()
+    const { hidden, cartItems } = useCart();
+    const [total, setTotal] = useState(0);
+
+    function calculateCartTotal() {
+        let cartTotal = 0;
+        cartItems.forEach(item => {
+            const storeItem = storeItems.find(storeItem => storeItem.id === item.id);
+            cartTotal += storeItem.price * item.quantity;
+        });
+        setTotal(cartTotal);
+    };
+
+    useEffect(() => {
+        calculateCartTotal();
+    }, [cartItems]);
+
+
     return (
         <div className={hidden ? "cart hide" : "cart show"}>
-            <h1>Cart</h1>
+            <h1 className='cart-title'>Cart</h1>
+            <h2 className='total'>Total: {total.toFixed(2)} $
+            </h2>
             {cartItems.map(item => {
                 return <CartItem key={item.id} id={item.id} quantity={item.quantity} storeItems={storeItems} />
             })}
--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useCart } from '../context/CartContext'
 import '../styles/Cart.css'
 function CartItem({ id, quantity, storeItems }) {
     const { removeFromCart } = useCart();
     const item = storeItems.find(item => item.id === id);
+
     if (item == null) return null;
     return (
         <div className='cart-item-cont'>
-            {console.log(item)}
             <img className='item-img-cart' src={item.image} alt="image" />
             <div className='title-price-cont'>
                 <h2 className='title'>{item.title} </h2>
