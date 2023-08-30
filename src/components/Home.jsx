bd0a7b9c0262b9a0dd273276d7d7d37afceff464
Yusef-H <yusefhalabi456@gmail.com>
2023-08-30 20:25:56 +0300

    add link to home page and improve positions

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../styles/home.css';
 import backgroundImage from '../assets/background.jpg';
+import { Link } from 'react-router-dom';
 function Home() {
   return <div className='home'>
     <img className="bg-img" src={backgroundImage} alt="" />
     <h1 className='home-title'>Discover a world of possibilities with our diverse range of electronics.</h1>
+    <Link className='home-shop' to='/store'>Shop Here!</Link>
   </div>
 }
 
